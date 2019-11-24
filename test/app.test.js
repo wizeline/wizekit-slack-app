3b57e9cee0331a19e7b7eff2048fda9a009ad31e
Duc Nguyen <vanducld@gmail.com>
2019-11-25 00:17:41 +0700

    add unit-test

--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,24 @@
 const request = require('supertest');
 const app = require('../app');
 
-
-describe('gae_node_request_example', () => {
-  describe('GET /', () => {
-    it('should get 200', (done) => {
+describe('test command from slack', () => {
+  describe('kudos command /', () => {
+    it('should return response_type only ', (done) => {
       request(app)
-        .get('/')
-        .expect(200, done);
+        .post('/commands/kudos-me')
+        .expect(200, {
+          response_type: 'in_channel',
+        }, done);
     });
 
-    it('should get Hello World', (done) => {
+    it('should get doesnt work message', (done) => {
       request(app)
-        .get('/')
-        .expect('Hello, world!', done);
+        .post('/commands/kudos-me')
+        .send('channel_name=directmessage&user_id=ABC')
+        .expect(200, {
+          response_type: 'in_channel',
+          text: 'Hi <@ABC>, `/kudos` doesn\'t work on Direct Message or Private Channel',
+        }, done);
     });
   });
 });
