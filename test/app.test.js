45ed10537b7da7379dd25799fe30e3e84b492d1b
Duc Nguyen <vanducld@gmail.com>
2019-12-22 22:19:12 +0700

    Add send direct message to notify receiver

--- test/app.test.js
+++ test/app.test.js
@@ -11,14 +11,12 @@ describe('test web endpoints', () => {
         .expect(200, { message: 'I\'m OK.' }, done);
     });
 
-    it('should get ephemeral message', (done) => {
+describe('test web endpoints', () => {
+  describe('health check /', () => {
+    it('should contains ok message ', (done) => {
       request(app)
-        .post('/commands/kudos-me')
-        .send('user_id=ABC&text=shorter than 20 msg.')
-        .expect(200, {
-          response_type: 'ephemeral',
-          text: 'Hi <@ABC>, `/kudos` is for encouraging people, please write something thoughtful.',
-        }, done);
+        .get('/api/healthcheck')
+        .expect(200, { message: 'I\'m OK.' }, done);
     });
   });
 });
