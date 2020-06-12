6ebff35ea537f88384222fda7cb639bf9d9219c9
Duc Nguyen <vanducld@gmail.com>
2020-06-12 09:34:25 +0700

    #27 - Refactor - Move poll code to PollService

--- src/service/poll-service.js
+++ src/service/poll-service.js
@@ -1,9 +1,23 @@
 const axios = require('axios').default;
 const stringUtil = require('../util/string-util');
 
+const BUTTON_LIST = [
+  ':one:',
+  ':two:',
+  ':three:',
+  ':four:',
+  ':five:',
+  ':six:',
+  ':seven:',
+  ':eight:',
+  ':nine:',
+  ':ten:',
+];
+
 async function proccessWizePoll(requestBody) {
   const { text, response_url, user_id } = requestBody;
-  const blocks = stringUtil.buildPoll(text, user_id);
+  const normalizedText = stringUtil.normalizeDoubleQuote(text);
+  const blocks = createPollMessage(normalizedText, user_id);
   return axios.post(response_url, {
     response_type: 'in_channel',
     blocks,
@@ -19,7 +33,11 @@ async function wizePollVote(requestBody) {
     (b) => b.accessory && b.accessory.action_id === actions[0].action_id,
   );
   const isIdentified = actions[0].action_id && actions[0].action_id.includes('identified');
-  const { text: votedText, votedList } = getVotedData(block, user.id, isIdentified);
+  const { text: votedText, votedList } = getVotedData(
+    block,
+    user.id,
+    isIdentified,
+  );
 
   block.text.text = votedText;
   if (votedList) {
@@ -69,16 +87,86 @@ function getTextWithCount(text, number) {
   if (!/`\d+`/.test(text)) {
     resultText = `${text} \`${number}\`.`;
   } else if (number > 0) {
-    resultText = text.replace(
-      /\s`\d+`\./,
-      ` \`${number}\`.`,
-    );
+    resultText = text.replace(/\s`\d+`\./, ` \`${number}\`.`);
   } else {
     resultText = text.replace(/\s`\d+`\./, '');
   }
   return resultText;
 }
 
+function createPollMessage(text, userId) {
+  const pollMeta = getPollMeta(text);
+  const removedFlag = text.substring(0, text.lastIndexOf('"'));
+  const sections = removedFlag.split('"').filter((t) => t.trim());
+  const question = sections.shift();
+  const pollBlocks = [];
+  if (pollMeta.isAnonymous) {
+    pollBlocks.push({
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text:
+            ':ghost: This poll is anonymous. The identity of all responses will be hidden. :see_no_evil: :hear_no_evil: :speak_no_evil:',
+        },
+      ],
+    });
+  }
+
+  pollBlocks.push({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*${question.charAt(0).toUpperCase() + question.slice(1)}*`,
+    },
+  });
+
+  for (let i = 0; i < sections.length; i += 1) {
+    const sectionText = sections[i];
+    const buttonText = BUTTON_LIST[i] || BUTTON_LIST[0];
+
+    const actionId = `${pollMeta.isSingle ? 'single' : 'multiple'}-${
+      pollMeta.isAnonymous ? 'anonymous' : 'identified'
+    }-${i}`;
+
+    pollBlocks.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `${buttonText} ${sectionText}`,
+      },
+      accessory: {
+        type: 'button',
+        style: 'primary',
+        text: {
+          type: 'plain_text',
+          text: buttonText,
+        },
+        action_id: actionId,
+      },
+    });
+  }
+
+  pollBlocks.push({
+    type: 'context',
+    elements: [
+      {
+        type: 'mrkdwn',
+        text: `Created by <@${userId}> with \`/wizepoll\` :rocket:`,
+      },
+    ],
+  });
+
+  return pollBlocks;
+}
+
+function getPollMeta(pollText) {
+  const flagTextOnly = pollText.replace(/"\w+"/gi, '');
+  return {
+    isSingle: /single/gi.test(flagTextOnly),
+    isAnonymous: /anonymous/gi.test(flagTextOnly),
+  };
+}
 
 module.exports = {
   proccessWizePoll,
