9ce4c8d64c254277c08ca4d08a1407642c6da22d
DUC NGUYEN <vanducld@gmail.com>
2021-01-11 14:40:18 +0700

    Change to ReactJS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ silent: true });
 const app = require('./src/server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
